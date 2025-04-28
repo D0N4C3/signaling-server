73fc6bd859ee6e17593e2442fa54a68265a499dd
D0N4C3 <adde215@gmail.com>
2025-04-28 22:27:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ const admin = require('firebase-admin');
 // ——————————
 // 2) Firebase Admin
 // ——————————
+
+// Parse SERVICE_ACCOUNT_KEY as JSON
 const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
+
+// Fix private key line breaks
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
